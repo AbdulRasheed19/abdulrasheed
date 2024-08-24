5061e39d4684a053d55a30829e185ea70656b423
AbdulRasheed19 <91790817+AbdulRasheed19@users.noreply.github.com>
2024-08-24 03:35:16 -0700

    Add files via upload

---
+++ abdulrasheed/container/footer.php
@@ -0,0 +1,8 @@
+
+  <footer>
+    <div class="container">
+      <div class="st-copyright-wrap text-center">
+        <div class="st-copyright-text">© 2024. Designed by Abdul Rasheed. All right reserved.</div>
+      </div>
+    </div>
+  </footer>
---
+++ abdulrasheed/container/head_links.php
@@ -0,0 +1,16 @@
+  <!-- Meta Tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Laralink">
+  <!-- Page Title -->
+  <title>Abdul Rasheed-Portfolio</title>
+  <!-- Favicon Icon -->
+  <link rel="icon" href="assets/img/favicon.png" />
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/css/fontawesome.css" />
+  <link rel="stylesheet" href="assets/css/slick.css" />
+  <link rel="stylesheet" href="assets/css/lightgallery.min.css" />
+  <link rel="stylesheet" href="assets/css/animate.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
