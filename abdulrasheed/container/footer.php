6a6062fc2c035b6d75edcf8f42620671575dcccf
AbdulRasheed19 <91790817+AbdulRasheed19@users.noreply.github.com>
2024-08-24 03:38:41 -0700

    Delete abdulrasheed directory

--- abdulrasheed/container/footer.php
+++
@@ -1,8 +0,0 @@
-
-  <footer>
-    <div class="container">
-      <div class="st-copyright-wrap text-center">
-        <div class="st-copyright-text">© 2024. Designed by Abdul Rasheed. All right reserved.</div>
-      </div>
-    </div>
-  </footer>
